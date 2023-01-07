5fd0745b78a914bc3f0066e8eb000830e374a8bb
stone1985 <igevdo@mail.ru>
2023-01-07 14:58:05 +0600

    - Data layer

---
+++ Botticelli.DataLayer.Interfaces/IStorage.cs
@@ -0,0 +1,13 @@
+﻿namespace Botticelli.DataLayer.Interfaces
+{
+    public interface IStorage<T, in TId>
+    where T : class
+    {
+        public T Get(TId id);
+        public ICollection<T> Get(Func<T, bool> filter);
+        public void Add(params T[] entites);
+        public void Remove(TId id);
+        public void Remove(T entity);
+        public void Update(params T[] entities);
+    }
+}
