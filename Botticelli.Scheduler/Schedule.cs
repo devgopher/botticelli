252032e30e36f1efa0e5cbb8199e5582ab87f5cb
stone1985 <igevdo@mail.ru>
2023-01-08 16:57:49 +0600

    - scheduler draft initial

---
+++ Botticelli.Scheduler/Schedule.cs
@@ -0,0 +1,10 @@
+﻿using Botticelli.Scheduler.Interfaces;
+
+namespace Botticelli.Scheduler
+{
+    public class Schedule : ISchedule
+    {
+        public string? Name { get; set; }
+        public string? Cron { get; set; }
+    }
+}
