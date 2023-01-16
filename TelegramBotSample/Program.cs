d119503f755f9b5c1bd4ce2c913c892ba97caf2c
stone1985 <igevdo@mail.ru>
2023-01-16 20:41:24 +0600

    - admin front initial project

--- TelegramBotSample/Program.cs
+++ TelegramBotSample/Program.cs
@@ -11,7 +11,6 @@ builder.Services.AddTelegramBot(new BotOptionsBuilder<TelegramBotSettings>()
     .Set(s => s.TelegramToken = "5746549361:AAFZcvuRcEk7QO4OfAjTYQQUeUpcaES3kqk")
     .Set(s => s.Name = "test_bot"));
 
-;
 //builder.Services.AddViberBot(new BotOptionsBuilder<ViberBotSettings>()
 //    .Set(s => s.ViberToken = "5065bdf5c527dfe8-3dfad317d974d1-ac5916e258fc1a93")
 //    .Set(s => s.Name = "test_bot"));
