bc61650664f3b13040d94accbc7a67f38c010661
stone1985 <igevdo@mail.ru>
2023-03-05 14:20:46 +0600

    - rabbit bus settings

---
+++ Botticelli.Bus.Rabbit/Settings/QueueSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace Botticelli.Bus.Rabbit.Settings;
+
+public class QueueSettings
+{
+    public string Name { get; set; }
+    public bool Durable { get; set; }
+
+    /// <summary>
+    /// Tries to create a queue (if false - you should create a queue by yorself)
+    /// </summary>
+    public bool TryCreate { get; set; }
+
+    /// <summary>
+    /// Checks if queue exists on publish. If TryCreate=true - tries to create it,
+    /// if false - throws an exception
+    /// </summary>
+    public bool CheckQueueOnPublish { get; set; }
+}
--- Botticelli.Bus.Rabbit/Settings/RabbitBusSettings.cs
+++ Botticelli.Bus.Rabbit/Settings/RabbitBusSettings.cs
@@ -3,6 +3,8 @@
 public class RabbitBusSettings
 {
     public string VHost { get; set; }
-    public string Exchange { get; set; }
     public string Uri { get; set; }
+    public string Exchange { get; set; }
+    public QueueSettings QueueSettings { get; set; }
+
 }
