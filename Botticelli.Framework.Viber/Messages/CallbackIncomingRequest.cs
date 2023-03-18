41077d4c28989ac129a4810266cb7e65bad888a0
stone1985 <igevdo@mail.ru>
2023-03-18 17:57:50 +0600

    - file-scoped namespace

--- Botticelli.Framework.Viber/Messages/CallbackIncomingRequest.cs
+++ Botticelli.Framework.Viber/Messages/CallbackIncomingRequest.cs
@@ -1,24 +1,23 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace Botticelli.Framework.Viber.Messages
+namespace Botticelli.Framework.Viber.Messages;
+
+/// <summary>
+/// Callback request (comes from Viber).
+/// Expected response only: 200 - OK.
+/// In other cases bot isn't available for Viber 
+/// </summary>
+public class CallbackIncomingRequest
 {
-    /// <summary>
-    /// Callback request (comes from Viber).
-    /// Expected response only: 200 - OK.
-    /// In other cases bot isn't available for Viber 
-    /// </summary>
-    public class CallbackIncomingRequest
-    {
-        [JsonPropertyName("event")]
-        public string Event { get; set; }
+    [JsonPropertyName("event")]
+    public string Event { get; set; }
 
-        [JsonPropertyName("timestamp")]
-        public long Timestamp { get; set; }
+    [JsonPropertyName("timestamp")]
+    public long Timestamp { get; set; }
 
-        [JsonPropertyName("message_token")]
-        public long MessageToken { get; set; }
+    [JsonPropertyName("message_token")]
+    public long MessageToken { get; set; }
 
-        [JsonPropertyName("user_id")]
-        public string UserId { get; set; }
-    }
+    [JsonPropertyName("user_id")]
+    public string UserId { get; set; }
 }
