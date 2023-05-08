5422bcbe8abc92d4a53290681128469a258ff9bd
stone1985 <igevdo@mail.ru>
2023-05-08 23:20:01 +0600

    - version and copyrights

--- Botticelli.Scheduler/Schedule.cs
+++
@@ -1,9 +0,0 @@
-﻿using Botticelli.Scheduler.Interfaces;
-
-namespace Botticelli.Scheduler;
-
-public class Schedule : ISchedule
-{
-    public string? Name { get; set; }
-    public string? Cron { get; set; }
-}
