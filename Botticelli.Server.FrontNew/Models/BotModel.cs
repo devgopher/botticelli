d9955304dfd14fad459874c71d7b86926897a228
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2024-01-26 23:05:16 +0300

    - messenger type field for AddBot

--- Botticelli.Server.FrontNew/Models/BotModel.cs
+++ Botticelli.Server.FrontNew/Models/BotModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Botticelli.Server.FrontNew.Models;
+﻿using Botticelli.Shared.Constants;
+
+namespace Botticelli.Server.FrontNew.Models;
 
 internal class BotModel
 {
     public string BotId { get; set; }
     public string BotKey { get; set; }
     public string BotName { get; set; }
+    
+    public string BotType { get; set; }
 }
