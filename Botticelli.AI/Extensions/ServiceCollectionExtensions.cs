ab696fe4956241276a297f9cf2ee9da8f79b528f
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2024-03-15 13:27:26 +0300

    - di fix

--- Botticelli.AI/Extensions/ServiceCollectionExtensions.cs
+++ Botticelli.AI/Extensions/ServiceCollectionExtensions.cs
@@ -8,6 +8,6 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddAiValidation(this IServiceCollection services)
     {
-        return services.AddValidatorsFromAssemblyContaining<AiMessage>();
+        return services.AddValidatorsFromAssemblyContaining<AiMessage>(ServiceLifetime.Singleton);
     }
 }
