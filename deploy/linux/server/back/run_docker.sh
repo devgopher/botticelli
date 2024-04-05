b1a65e38125d91010ae40b635f0a434648935166
stone1985 <igevdo@mail.ru>
2024-04-05 16:44:30 +0300

    - fix

--- deploy/linux/server/back/run_docker.sh
+++ deploy/linux/server/back/run_docker.sh
@@ -1,7 +1,8 @@
 sudo docker run -d -t -i -p 5042:5000 -p 7247:5001 \
+-v /data:/data \
 -e ASPNETCORE_URLS="https://0.0.0.0:5001;http://0.0.0.0:5000" \
 -e ASPNETCORE_ENVIRONMENT="Release" \
--e SecureStorageSettings__ConnectionString="Filename=database.db;Connection=shared;Password=$db_password" \
+-e SecureStorageSettings__ConnectionString="Filename=/data/database.db;Connection=shared;Password=$db_password" \
 -e ServerSettings__TokenLifetimeMin=1000 \
 -e ServerSettings__SmtpClientOptions__Server="$email_smtp_server" \
 -e ServerSettings__SmtpClientOptions__Port="$email_smtp_port" \
--- deploy/linux/server/front/run_standalone.sh
+++ deploy/linux/server/front/run_standalone.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y dotnet-sdk-7.0 dotnet-runtime-7.0 aspnetcore-runtime-7.0
 rm -rf botticelli/
 git clone https://github.com/devgopher/botticelli.git
 pushd botticelli/
-git checkout release/0.3
+git checkout dev/0.4
 git pull
 
 pushd Botticelli.Server.FrontNew
@@ -25,6 +25,6 @@ export BackSettings__BackUrl=$backend_url/v1/
 export BackSettings__AnalyticsUrl=$analytics_url/v1/
 
 
-nohup dotnet run Botticelli.Server.FrontNew.csproj >/dev/null 2>&1
+nohup dotnet run Botticelli.Server.FrontNew.csproj >/dev/null 2>&1 &
 
 popd
