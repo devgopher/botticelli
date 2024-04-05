6cc339f0cbf64a56981b60d31161d8fc661671fd
stone1985 <igevdo@mail.ru>
2024-04-05 17:03:42 +0300

    - fix

--- Botticelli/Program.cs
+++ Botticelli/Program.cs
@@ -30,7 +30,6 @@ builder.Services.AddCors(o => o.AddDefaultPolicy(builder =>
 
 
 builder.Configuration
-    .AddJsonFile("appsettings.json")
     .AddEnvironmentVariables();
 
 var secureStorageSettings = builder.Configuration
