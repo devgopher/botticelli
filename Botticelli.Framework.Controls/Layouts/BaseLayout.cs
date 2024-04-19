f6bea6590358f9d547bafd287644fd65d7016c05
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2024-04-19 16:11:17 +0300

    - fix

--- Botticelli.Framework.Controls/Layouts/BaseLayout.cs
+++ Botticelli.Framework.Controls/Layouts/BaseLayout.cs
@@ -4,8 +4,7 @@ public class BaseLayout : ILayout
 {
     private readonly List<Row> _rows = new(5);
   
-    public void AddRow(Row row)
-    {
-        _rows.Add(row);
-    }
+    public void AddRow(Row row) => _rows.Add(row);
+
+    public IEnumerable<Row> Rows => _rows;
 }
