c3c28895d43cd90a516e9a3b41f50f6171ccbe0b
stone1985 <igevdo@mail.ru>
2024-06-22 22:27:43 +0300

    BOTTICELLI-44: Wait for client input cmd

--- Botticelli.Framework/Commands/Processors/WaitForClientResponseCommandChainProcessor.cs
+++ Botticelli.Framework/Commands/Processors/WaitForClientResponseCommandChainProcessor.cs
@@ -1,15 +1,22 @@
 using Botticelli.Client.Analytics;
 using Botticelli.Framework.Commands.Validators;
+using Botticelli.Framework.Exceptions;
 using Botticelli.Shared.ValueObjects;
 using Microsoft.Extensions.Logging;
 
 namespace Botticelli.Framework.Commands.Processors;
 
+/// <summary>
+/// A command with waiting for a response
+/// </summary>
+/// <typeparam name="TInputCommand"></typeparam>
 public abstract class WaitForClientResponseCommandChainProcessor<TInputCommand> : CommandProcessor<TInputCommand>, ICommandChainProcessor<TInputCommand>
         where TInputCommand : class, ICommand
 {
-    private Dictionary<string, Message> _receivedMessages = new(100);
-    
+    private readonly Dictionary<string, Message> _receivedMessages = new(100);
+    private TimeSpan Timeout { get; set; } = TimeSpan.FromMinutes(1);
+
+
     protected WaitForClientResponseCommandChainProcessor(ILogger<CommandChainProcessor<TInputCommand>> logger,
                                                          ICommandValidator<TInputCommand> validator,
                                                          MetricsProcessor metricsProcessor) : base(logger, validator, metricsProcessor)
@@ -19,26 +26,41 @@ public abstract class WaitForClientResponseCommandChainProcessor<TInputCommand> 
         {
             foreach (var chatId in args.Message?.ChatIds)
             {
-                _receivedMessages[chatId] = args.Message;
-                
-                // Sort by LastModified field 
-                args.Message.LastModifiedAt
-                
+                if (_receivedMessages[chatId].LastModifiedAt < args.Message?.LastModifiedAt)
+                    _receivedMessages[chatId] = args.Message;
             }
         };
-
-
     }
     
     public override async Task ProcessAsync(Message message, CancellationToken token)
     {
         await base.ProcessAsync(message, token);
+        
+        var started = DateTime.Now;
+        Message responseMessage = null;
+        
+        while (DateTime.Now - started > Timeout)
+        {
+            var chatId = message.ChatIds.First()!;
+            if (_receivedMessages[chatId].LastModifiedAt <= message.LastModifiedAt) 
+                continue;
 
+            responseMessage = _receivedMessages[chatId];
+            break;
+        }
 
-        await Next.ProcessAsync(message, token);
+        if (responseMessage == null)
+        {
+            _logger.LogInformation("No response from a client for message {uid}", message.Uid);
+
+            return;
+        }
+        
+        if (Next != null)
+            await Next.ProcessAsync(responseMessage, token);
+        else
+            _logger.LogInformation("No Next command for message {uid}", message.Uid);
     }
-
-    
     
     public ICommandChainProcessor Next { get; set; }
 }
