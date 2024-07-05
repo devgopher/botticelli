6ae71c76741d13b28530f735909899e2cdcc1cc8
stone1985 <igevdo@mail.ru>
2024-07-05 22:55:23 +0300

    cleanup

--- Botticelli.LoadTests.Receiver/ILoadTestGate.cs
+++ Botticelli.LoadTests.Receiver/ILoadTestGate.cs
@@ -1,15 +1,14 @@
-using Botticelli.Bot.Interfaces.Processors;
 using Botticelli.LoadTests.Receiver.Result;
 
 namespace Botticelli.LoadTests.Receiver;
 
 /// <summary>
-/// A load tests gate allows access of a load test tool to bot's commands
+///     A load tests gate allows access of a load test tool to bot's commands
 /// </summary>
 public interface ILoadTestGate
 {
     /// <summary>
-    /// Throws a command and returns a message uid in order to track a response
+    ///     Throws a command and returns a message uid in order to track a response
     /// </summary>
     /// <param name="command"></param>
     /// <param name="args"></param>
@@ -18,7 +17,7 @@ public interface ILoadTestGate
     public Task ThrowCommand(string command, string args, CancellationToken token);
 
     /// <summary>
-    /// Waits response for a command
+    ///     Waits response for a command
     /// </summary>
     /// <param name="task"></param>
     /// <param name="timeout"></param>
