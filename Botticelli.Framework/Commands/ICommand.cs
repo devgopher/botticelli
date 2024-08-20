3e93334b85c473a1d52a199bed4c260aaa0e8a9f
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2024-08-20 12:56:31 +0300

    - static

--- Botticelli.Framework/Commands/ICommand.cs
+++ Botticelli.Framework/Commands/ICommand.cs
@@ -3,5 +3,5 @@
 public interface ICommand
 {
     Guid Id { get; }
-    string CommandName => GetType().Name.Replace("Command", string.Empty);
+    static string CommandName => GetType().Name.Replace("Command", string.Empty);
 }
