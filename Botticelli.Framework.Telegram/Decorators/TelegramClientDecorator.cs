4ad017b21697c6144b70c6bc57bc50ab7858e571
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2024-08-26 13:30:41 +0300

    BOTTICELLI-46: throttler

--- Botticelli.Framework.Telegram/Decorators/TelegramClientDecorator.cs
+++ Botticelli.Framework.Telegram/Decorators/TelegramClientDecorator.cs
@@ -8,6 +8,22 @@ using Telegram.Bot.Requests.Abstractions;
 
 namespace Botticelli.Framework.Telegram.Decorators;
 
+public class Throttler
+{
+    private static readonly TimeSpan Delay = TimeSpan.FromSeconds(5);
+    private DateTime _prevDt = DateTime.MinValue;
+    
+    public Task Throttle(CancellationToken ct)
+    {
+        var diff = DateTime.UtcNow - _prevDt;
+        if (diff < Delay)
+            return Task.Delay(Delay - diff, ct);
+
+        _prevDt = DateTime.UtcNow;
+        return Task.CompletedTask;
+    }
+}
+
 /// <summary>
 /// A Telegram.Bot decorator with auto-retry on 429 error
 /// </summary>
@@ -16,17 +32,20 @@ public class TelegramClientDecorator : ITelegramBotClient
     private readonly TelegramBotClient _bot;
     private readonly RetryPolicy _policy = Policy
         .Handle<ApiRequestException>(e => e.ErrorCode == (int)HttpStatusCode.TooManyRequests)
-        .WaitAndRetry(10, (i , _) => TimeSpan.FromMilliseconds(10000 * Math.Exp(i)));
+        .WaitAndRetry(10, (i , _) => TimeSpan.FromSeconds(10 * Math.Exp(i)));
+
+    private readonly Throttler _throttler = new Throttler();
     
     public TelegramClientDecorator(string token,
         HttpClient? httpClient = null) =>
         _bot = new TelegramBotClient(token, httpClient);
 
     public async Task<TResponse> MakeRequestAsync<TResponse>(IRequest<TResponse> request,
-        CancellationToken cancellationToken = new CancellationToken())
+        CancellationToken cancellationToken = new())
     {
         try
         {
+            await _throttler.Throttle(cancellationToken);
             return await _policy.Execute(() => _bot.MakeRequestAsync(request, cancellationToken)).ConfigureAwait(false);
         }
         catch (ApiRequestException ex)
