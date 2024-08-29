82a61399807b0eb43bc1966cd0723aaf44a76e89
stone1985 <igevdo@mail.ru>
2024-08-29 14:16:53 +0300

    - new dockerfiles

--- Botticelli.Framework.Telegram/Extensions/ServiceCollectionExtensions.cs
+++ Botticelli.Framework.Telegram/Extensions/ServiceCollectionExtensions.cs
@@ -1,23 +1,12 @@
-﻿using Botticelli.BotBase;
-using Botticelli.BotBase.Settings;
-using Botticelli.BotBase.Utils;
-using Botticelli.Client.Analytics;
-using Botticelli.Framework.Controls.Parsers;
-using Botticelli.Framework.Extensions;
+﻿using Botticelli.Framework.Controls.Parsers;
 using Botticelli.Framework.Options;
 using Botticelli.Framework.Telegram.Builders;
 using Botticelli.Framework.Telegram.Decorators;
-using Botticelli.Framework.Telegram.Handlers;
-using Botticelli.Framework.Telegram.HostedService;
 using Botticelli.Framework.Telegram.Layout;
 using Botticelli.Framework.Telegram.Options;
 using Botticelli.Interfaces;
-using Botticelli.Shared.ValueObjects;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Telegram.Bot;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Botticelli.Framework.Telegram.Extensions;
@@ -30,11 +19,12 @@ public static class ServiceCollectionExtensions
     /// <param name="services"></param>
     /// <param name="config"></param>
     /// <param name="optionsBuilder"></param>
+    /// <param name="telegramClientBuilder"></param>
     /// <returns></returns>
     public static IServiceCollection AddTelegramBot(this IServiceCollection services,
-        IConfiguration config,
-        BotOptionsBuilder<TelegramBotSettings> optionsBuilder,
-        TelegramClientDecoratorBuilder telegramClientBuilder)
+                                                    IConfiguration config,
+                                                    BotOptionsBuilder<TelegramBotSettings> optionsBuilder,
+                                                    TelegramClientDecoratorBuilder telegramClientBuilder)
     {
         var bot = TelegramBotBuilder.Instance(services, optionsBuilder, config)
             .AddStorage()
