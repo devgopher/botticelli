f55f09e7a504814e63c379fe80c138c6f72ddc7e
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2025-02-23 12:07:38 +0300

    - increased cache duration

--- Botticelli.Framework.Telegram/Handlers/BotUpdateHandler.cs
+++ Botticelli.Framework.Telegram/Handlers/BotUpdateHandler.cs
@@ -19,7 +19,7 @@ public class BotUpdateHandler : IBotUpdateHandler
     private static readonly MemoryCache Cache = MemoryCache.Default;
     private static readonly CacheItemPolicy CacheItemPolicy = new()
     {
-        AbsoluteExpiration = DateTime.Now.AddDays(1)
+        SlidingExpiration = TimeSpan.FromDays(1)
     };
 
     public BotUpdateHandler(ILogger<BotUpdateHandler> logger, ClientProcessorFactory processorFactory)
