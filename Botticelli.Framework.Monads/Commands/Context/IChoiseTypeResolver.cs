2da945c7886d8fd936550a1f6c9b0c180dcf0796
stone1985 <igevdo@mail.ru>
2025-03-02 13:02:51 +0300

    - multichain postponed: https://github.com/devgopher/botticelli/tree/dev/0.7_multichain_postponed

--- Botticelli.Framework.Monads/Commands/Context/IChoiseTypeResolver.cs
+++
@@ -1,8 +0,0 @@
-using Botticelli.Framework.Monads.Commands.Processors.Multichain;
-
-namespace Botticelli.Framework.Monads.Commands.Context;
-
-public interface IChoiseResolver
-{
-    public bool Resolve(IChoise choise);
-}
