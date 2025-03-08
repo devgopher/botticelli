03dc4a79faad82f9571771d027718a237afd9ea4
stone1985 <ievdokimov.csharp@gmail.com>
2025-03-08 13:25:05 +0300

    BOTTICELLI-57: - sqlite support

---
+++ Botticelli.Auth.Data.Sqlite/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Botticelli.Auth.Extensions;
+using Botticelli.Auth.Shared.Settings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Botticelli.Auth.Data.Sqlite;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds a basic authorization implementation
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="config"></param>
+    /// <param name="dbParameters"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddSqliteBasicBotUserAuth(this IServiceCollection services, IConfiguration config, Action<DbContextOptionsBuilder>? dbParameters = null)
+    {
+        services.AddBasicBotUserAuth();
+        
+        var settings = config.GetSection(nameof(AuthSettings))
+            .Get<AuthSettings>();
+       
+        return services.AddDbContext<AuthDefaultDbContext>(opt =>
+            {
+                dbParameters?.Invoke(opt);
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                    .UseSqlite(settings!.ConnectionString);
+            },
+            ServiceLifetime.Singleton);
+    }
+}
