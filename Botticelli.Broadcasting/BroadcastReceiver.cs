c4b3e48b57031f84237665c46482fd27920198e0
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2025-09-27 12:10:04 +0300

    - retry policy for BroadcastReceived request

--- Botticelli.Broadcasting/BroadcastReceiver.cs
+++ Botticelli.Broadcasting/BroadcastReceiver.cs
@@ -10,6 +10,8 @@ using Botticelli.Shared.API.Client.Responses;
 using Flurl.Http;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Polly;
+
 namespace Botticelli.Broadcasting;
 
 /// <summary>
@@ -24,7 +26,9 @@ public class BroadcastReceiver<TBot> : IHostedService
     private readonly IBot _bot;
     private readonly BroadcastingContext _context;
     private readonly TimeSpan _longPollTimeout = TimeSpan.FromSeconds(30);
+    private readonly TimeSpan _broadcastReceivedTimeout = TimeSpan.FromSeconds(10);
     private readonly TimeSpan _retryPause = TimeSpan.FromMilliseconds(150);
+    private const int RetryCount = 3;
     private readonly BroadcastingSettings _settings;
     private readonly ILogger<BroadcastReceiver<TBot>> _logger;
     public CancellationTokenSource CancellationTokenSource { get; private set; }
@@ -111,18 +115,22 @@ public class BroadcastReceiver<TBot> : IHostedService
 
     private async Task<BroadCastMessagesReceivedResponse?> SendBroadcastReceived(List<string> chatIds, CancellationToken cancellationToken)
     {
-        var updatesResponse = await $"{_settings.ServerUri}/bot/client/BroadcastReceived"
-                                    .WithTimeout(_longPollTimeout)
-                                    .PostJsonAsync(new BroadCastMessagesReceivedRequest
-                                                   {
-                                                       BotId = _settings.BotId,
-                                                       MessageIds = chatIds.ToArray()
-                                                   },
-                                                   cancellationToken: cancellationToken);
+        var response = Policy
+            .Handle<HttpRequestException>() // Handle network-related exceptions
+            .OrResult<IFlurlResponse>(r => !r.ResponseMessage.IsSuccessStatusCode) // Handle non-success status codes
+            .WaitAndRetryAsync(RetryCount, i => _retryPause.Multiply(10 * i))
+            .ExecuteAsync(async () => await $"{_settings.ServerUri}/bot/client/BroadcastReceived"
+                .WithTimeout(_broadcastReceivedTimeout)
+                .PostJsonAsync(new BroadCastMessagesReceivedRequest
+                    {
+                        BotId = _settings.BotId,
+                        MessageIds = chatIds.ToArray()
+                    },
+                    cancellationToken: cancellationToken));
 
-        if (!updatesResponse.ResponseMessage.IsSuccessStatusCode) return null;
-
-        return await updatesResponse.ResponseMessage.Content
-                                    .ReadFromJsonAsync<BroadCastMessagesReceivedResponse>(cancellationToken);
+        return await response.Result
+            .ResponseMessage
+            .Content
+            .ReadFromJsonAsync<BroadCastMessagesReceivedResponse>(cancellationToken);
     }
 }
