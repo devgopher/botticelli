2561427cc35d154e7dd99d5fd527bbb475eeee95
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2025-10-06 21:59:41 +0300

    - cosmetic

--- Botticelli.Server.FrontNew/Utils/ProgressStreamContent.cs
+++ Botticelli.Server.FrontNew/Utils/ProgressStreamContent.cs
@@ -2,29 +2,24 @@ using System.Net;
 
 namespace Botticelli.Server.FrontNew.Utils;
 
-public class ProgressStreamContent : StreamContent
+public class ProgressStreamContent(Stream content, Action<long, long> progress) : StreamContent(content)
 {
-    private readonly Action<long, long> _progress;
-
-    public ProgressStreamContent(Stream content, Action<long, long> progress) : base(content)
-    {
-        _progress = progress;
-    }
-
-    protected override async Task SerializeToStreamAsync(Stream stream, TransportContext context)
+    private const int BufferSize = 8192;
+    
+    protected override async Task SerializeToStreamAsync(Stream stream, TransportContext? context)
     {
         var totalBytes = Headers.ContentLength ?? -1;
-        var buffer = new byte[8192];
+        var buffer = new byte[BufferSize];
         long totalRead = 0;
         int bytesRead;
 
         var contentStream = await ReadAsStreamAsync();
         contentStream.Seek(0, SeekOrigin.Begin);
-        while ((bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+        while ((bytesRead = await contentStream.ReadAsync(buffer)) > 0)
         {
-            await stream.WriteAsync(buffer, 0, bytesRead);
+            await stream.WriteAsync(buffer.AsMemory(0, bytesRead));
             totalRead += bytesRead;
-            _progress?.Invoke(totalRead, totalBytes);
+            progress?.Invoke(totalRead, totalBytes);
         }
     }
 }
