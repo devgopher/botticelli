05fb53172cd7c03c3f6deb5f1c8525eb8fbd225d
stone1985 <igevdo@mail.ru>
2025-10-28 18:36:33 +0300

    BOTTICELLI-69: - unit tests for getting command arguments

---
+++ Tests/Botticelli.Framework.Tests/Commands/Utils/CommandUtilsTest.cs
@@ -0,0 +1,113 @@
+using Botticelli.Framework.Commands.Utils;
+using NUnit.Framework;
+// ReSharper disable InvokeAsExtensionMethod
+
+namespace Botticelli.Framework.Tests.Commands.Utils;
+
+[TestFixture]
+public class CommandUtilsTests
+{
+    [Test]
+    public void GetArguments_NullInput_ReturnsEmptyString()
+    {
+        // Arrange
+        string? input = null;
+
+        // Act
+        var result = CommandUtils.GetArguments(input);
+
+        // Assert
+        Assert.AreEqual(string.Empty, result);
+    }
+
+    [Test]
+    public void GetArguments_EmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        var input = string.Empty;
+
+        // Act
+        var result = CommandUtils.GetArguments(input);
+
+        // Assert
+        Assert.AreEqual(string.Empty, result);
+    }
+
+    [Test]
+    public void GetArguments_SingleCommand_ReturnsEmptyString()
+    {
+        // Arrange
+        var input = "/command";
+
+        // Act
+        var result = CommandUtils.GetArguments(input);
+
+        // Assert
+        Assert.AreEqual(string.Empty, result);
+    }
+
+    [Test]
+    public void GetArguments_CommandWithArguments_ReturnsArguments()
+    {
+        // Arrange
+        var input = "/command argument1 argument2";
+
+        // Act
+        var result = CommandUtils.GetArguments(input);
+
+        // Assert
+        Assert.AreEqual("argument1 argument2", result);
+    }
+
+    [Test]
+    public void GetArguments_OnlyCommandAndWhitespace_ReturnsEmptyString()
+    {
+        // Arrange
+        var input = "/command     ";
+
+        // Act
+        var result = CommandUtils.GetArguments(input);
+
+        // Assert
+        Assert.AreEqual(string.Empty, result);
+    }
+
+    [Test]
+    public void GetArguments_CommandWithMixedCase_ReturnsArguments()
+    {
+        // Arrange
+        var input = "/Command Argument1 Argument2";
+
+        // Act
+        var result = CommandUtils.GetArguments(input);
+
+        // Assert
+        Assert.AreEqual("Argument1 Argument2", result);
+    }
+
+    [Test]
+    public void GetArguments_TrailingSpacesInArguments_ReturnsArgumentsWithoutTrailingSpaces()
+    {
+        // Arrange
+        var input = "/command argument1 argument2     ";
+
+        // Act
+        var result = CommandUtils.GetArguments(input);
+
+        // Assert
+        Assert.AreEqual("argument1 argument2", result);
+    }
+
+    [Test]
+    public void GetArguments_MultipleSpacesBetweenArguments_ReturnsArguments()
+    {
+        // Arrange
+        var input = "/command   arg1    arg2";
+
+        // Act
+        var result = CommandUtils.GetArguments(input);
+
+        // Assert
+        Assert.AreEqual("arg1 arg2", result);
+    }
+}
