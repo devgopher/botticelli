e19b04ea39794ef9f0a00aa42900605eef48546a
stone1985 <igevdo@mail.ru>
2025-11-27 22:30:04 +0300

    - validation for Body and CallbackData!

--- Botticelli.Shared/Validation/MessageValidator.cs
+++ Botticelli.Shared/Validation/MessageValidator.cs
@@ -7,7 +7,16 @@ public class MessageValidator : AbstractValidator<Message>
 {
     public MessageValidator()
     {
-        //RuleFor(x => x.Body).NotEmpty();
+        RuleFor(x => x.Body)
+                .NotEmpty()
+                .When(x => string.IsNullOrWhiteSpace(x.CallbackData))
+                .WithMessage("Body is empty: body should be provided when CallbackData is null or empty!");
+        
+        RuleFor(x => x.CallbackData)
+                .NotEmpty()
+                .When(x => string.IsNullOrWhiteSpace(x.Body))
+                .WithMessage("Callback data is empty: callback data should be provided when Body is null or empty!");;
+        
         RuleFor(x => x.Uid).NotEmpty();
     }
 }
