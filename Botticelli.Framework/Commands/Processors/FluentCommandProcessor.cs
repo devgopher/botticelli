b64d30d8e04d8a87117149354447aadda413ddfc
Igor Evdokimov <ievdokimov.csharp@gmail.com>
2026-01-31 12:58:01 +0300

    Fluent command processor: - processing location/contact/polls

--- Botticelli.Framework/Commands/Processors/FluentCommandProcessor.cs
+++ Botticelli.Framework/Commands/Processors/FluentCommandProcessor.cs
@@ -12,28 +12,28 @@ using Microsoft.Extensions.Logging;
 namespace Botticelli.Framework.Commands.Processors;
 
 public abstract class FluentCommandProcessor<TCommand>(
-        ILogger logger,
-        MetricsProcessor metricsProcessor,
-        ICommandValidator<TCommand> commandValidator)
-        : ICommandProcessor
-        where TCommand : class, IFluentCommand
+    ILogger logger,
+    MetricsProcessor metricsProcessor,
+    ICommandValidator<TCommand> commandValidator)
+    : ICommandProcessor
+    where TCommand : class, IFluentCommand
 {
     protected IBot? Bot;
-    
+
     public abstract string CommandText { get; }
 
     public async Task ProcessAsync(Message message, CancellationToken token)
     {
         logger.LogDebug("{processorName}.ProcessAsync() : processing a message {messageUid}: {message}",
-                        nameof(FluentCommandProcessor<TCommand>),
-                        message.Uid,
-                        JsonSerializer.Serialize(message));
+            nameof(FluentCommandProcessor<TCommand>),
+            message.Uid,
+            JsonSerializer.Serialize(message));
 
         if (!CheckCommand(message))
         {
             logger.LogDebug("{processorName}.ProcessAsync() : processing a message {messageUid}: failed",
-                            nameof(FluentCommandProcessor<TCommand>),
-                            message.Uid);
+                nameof(FluentCommandProcessor<TCommand>),
+                message.Uid);
 
             return;
         }
@@ -41,8 +41,8 @@ public abstract class FluentCommandProcessor<TCommand>(
         if (await commandValidator.Validate(message))
         {
             logger.LogDebug("{processorName}.ProcessAsync() : processing a message {messageUid}: command is valid",
-                            nameof(FluentCommandProcessor<TCommand>),
-                            message.Uid);
+                nameof(FluentCommandProcessor<TCommand>),
+                message.Uid);
             SendMetric();
             await InnerProcess(message, token);
         }
@@ -57,10 +57,12 @@ public abstract class FluentCommandProcessor<TCommand>(
             };
 
             logger.LogDebug("{processorName}.ProcessAsync() : processing a message {messageUid}: command is NOT valid!",
-                            nameof(FluentCommandProcessor<TCommand>),
-                            message.Uid);
+                nameof(FluentCommandProcessor<TCommand>),
+                message.Uid);
 
-            await Bot.SendMessageAsync(errMessageRequest, token);
+            if (message.Location != null) await InnerProcessLocation(message, token);
+            if (message.Poll != null) await InnerProcessPoll(message, token);
+            if (message.Contact != null) await InnerProcessContact(message, token);
         }
     }
 
@@ -83,7 +85,22 @@ public abstract class FluentCommandProcessor<TCommand>(
     {
         logger.LogDebug("{processorName}.SendMetric() : sending a metric...", nameof(FluentCommandProcessor<TCommand>));
         metricsProcessor.Process($"{GetType().Name.Replace("Processor", string.Empty)}Command",
-                                 BotDataUtils.GetBotId()!);
+            BotDataUtils.GetBotId()!);
+    }
+
+    protected virtual Task InnerProcessContact(Message message, CancellationToken token)
+    {
+        return Task.CompletedTask;
+    }
+
+    protected virtual Task InnerProcessPoll(Message message, CancellationToken token)
+    {
+        return Task.CompletedTask;
+    }
+
+    protected virtual Task InnerProcessLocation(Message message, CancellationToken token)
+    {
+        return Task.CompletedTask;
     }
 
     protected async Task SendMessage<TReplyMarkup>(SendMessageRequest request,
