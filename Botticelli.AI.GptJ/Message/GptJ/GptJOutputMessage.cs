ffd9ad3698f7cbdac8fd1740fb0748f98732826e
Igor Evdokimov <ievdokimov.csharp@gmail.com>
2026-02-15 20:32:04 +0300

    - gpt-j was removed

--- Botticelli.AI.GptJ/Message/GptJ/GptJOutputMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace Botticelli.AI.GptJ.Message.GptJ;
-
-public class GptJOutputMessage
-{
-    [JsonProperty("completion")]
-    public string Completion { get; set; }
-}
